9fec92a918a156d8d041c9807e5dbfebf9d0bec1
Stefan <mycourseresource@users.noreply.github.com>
2020-07-08 12:40:37 +0100

    Create index.php

---
+++ PHP Files/index.php
@@ -0,0 +1,31 @@
+<?php
+
+$html ='<div id="wrapper"><h3>Test</h3></div>';
+
+if (get_client_ip() == 'your IP address' || get_client_ip() == '46.7.62.182-') {
+  echo $html;
+}
+
+//else {exit;}
+
+
+// Function to get the client IP address
+function get_client_ip() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+?>

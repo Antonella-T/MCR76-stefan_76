a3ec899ac4f9c953643d530055756af1358d3564
Stefan <mycourseresource@users.noreply.github.com>
2020-07-20 15:21:35 +0100

    Update index.php

--- PHP Files/index.php
+++ PHP Files/index.php
@@ -1,35 +1,19 @@
 <?php
-
-$myRoot = substr($_SERVER['DOCUMENT_ROOT'], 0, strpos($_SERVER['DOCUMENT_ROOT'], 'public_html'));
-echo '<script>console.log(\'My Root: ' . $myRoot . '\');</script>';
-
-
-$html ='<div id="wrapper"><h3>Test</h3></div>';
-
-if (get_client_ip() == 'your IP address' || get_client_ip() == '46.7.62.182-') {
-  echo $html;
-}
-
-//else {exit;}
-
-
-// Function to get the client IP address
-function get_client_ip() {
-    $ipaddress = '';
-    if (isset($_SERVER['HTTP_CLIENT_IP']))
-        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED'];
-    else if(isset($_SERVER['REMOTE_ADDR']))
-        $ipaddress = $_SERVER['REMOTE_ADDR'];
-    else
-        $ipaddress = 'UNKNOWN';
-    return $ipaddress;
-}
+  $myRoot = substr($_SERVER['DOCUMENT_ROOT'], 0, strpos($_SERVER['DOCUMENT_ROOT'], 'public_html'));
+  $myPage = $_SERVER['PHP_SELF'];
+  require  $myRoot . 'mcr76_hidden/script.php';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>MCR76 - Parts DB</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.0/angular.js"></script> 
+  <script src="scripts/scripts.js.php"></script>  
+</head>
+<body>
+  <div class="wrapper"></div>
+    <h1>Welcome to Parts DB Page</h1>
+  </div><!-- wrapper end -->
+</body>

79f91cf3eac49a602128f0fa492043e513ee359a
Stefan <mycourseresource@users.noreply.github.com>
2020-07-27 12:36:01 +0100

    Update scripts.js.php

--- PHP Files/scripts.js.php
+++ PHP Files/scripts.js.php
@@ -2,5 +2,6 @@
   $myRoot = substr($_SERVER['DOCUMENT_ROOT'], 0, strpos($_SERVER['DOCUMENT_ROOT'], 'public_html'));
   $myPage = $_SERVER['PHP_SELF'];
   require  $myRoot . 'mcr76_hidden/script.php';
+  header('Content-Type: application/javascript');
 ?>
 console.log('Hello World!');

483ab41f285e2f5a54d755cd98b909667f8bf85d
Stefan <mycourseresource@users.noreply.github.com>
2020-07-28 11:27:50 +0100

    Create styles.css.php

---
+++ PHP Files/styles.css.php
@@ -0,0 +1,7 @@
+<?php
+  $myRoot = substr($_SERVER['DOCUMENT_ROOT'], 0, strpos($_SERVER['DOCUMENT_ROOT'], 'public_html'));
+  $myPage = $_SERVER['PHP_SELF'];
+  require  $myRoot . 'mcr76_hidden/script.php';
+  header('Content-Type: text/css');
+?>
+/* CSS File Rev 0.1 */

06f0300125b12e5d4244b443045559b80570bdf4
Stefan <mycourseresource@users.noreply.github.com>
2020-07-31 09:20:55 +0100

    Update index.php

--- PHP Files/fruit_demo/index.php
+++ PHP Files/fruit_demo/index.php
@@ -1,15 +1,15 @@
-<?php
-  $myRoot = substr($_SERVER['DOCUMENT_ROOT'], 0, strpos($_SERVER['DOCUMENT_ROOT'], 'public_html'));
-  $myPage = $_SERVER['PHP_SELF'];
-  require  $myRoot . 'mcr76_hidden/script.php';
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <?php
+    $myRoot = substr($_SERVER['DOCUMENT_ROOT'], 0, strpos($_SERVER['DOCUMENT_ROOT'], 'public_html'));
+    $myPage = $_SERVER['PHP_SELF'];
+    require  $myRoot . 'mcr76_hidden/script.php';
+  ?>
   <meta charset="utf-8">
   <title>MCR76 - Fruit DB</title>
   <link rel="stylesheet" type="text/css" href="css/styles.css.php">
-  <script>console.log('Home Page Rev 0.4')</script>
+  <script>console.log('Home Page Rev 0.5')</script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.0/angular.js"></script> 
   <script src="scripts/scripts.js.php"></script>  
@@ -64,8 +64,8 @@
       <label for="ngReadDoILike">Like:</label>
       <select id="ngReadDoILike" ng-model="ngReadDoILike">
         <option value="n/a">Don't Care</option>
-        <option value="true">Yes</option>
-        <option value="false">No</option>
+        <option value=1>Yes</option>
+        <option value=0>No</option>
       </select>
     </div><div>
       <label for="ngReadPopularity">Popularity:</label>
@@ -104,6 +104,28 @@
     </table>
 
     
+    <h2>Read with Autosuggest (AngularJS + &lt;datalist>)</h2>
+    <div>
+      <span>{{ngReadAutoMessageDL}}</span><br>
+      <label for="ngReadAutoDL">Search:</label>
+      <input list="ngReadAutoDLDataList" type="text" id="ngReadAutoDL" ng-model="ngReadAutoDL" ng-change="ngReadAutoRecordDL();"  ng-keyup="ngReadAutoShowDetailsDL()">
+      <datalist id="ngReadAutoDLDataList">
+        <option ng-repeat="row in ngReadAutoRecordDataDL | filter: ngReadAutoDL | orderBy:fruit" data-value="{{row.id}}">{{row.fruit}}</option>
+      </datalist>
+    </div>
+    <table border="1" ng-show="showNgReadAutoResultsDL">
+      <tr><th>Id</th><th>Fruit</th><th>Colour</th><th>doILike</th><th>Popularity</th><th>createDate</th><th>updateDate</th></tr>
+      <tr>
+        <td>{{ngReadAutoRecordDetailsDL.id}}</td>
+        <td>{{ngReadAutoRecordDetailsDL.fruit}}</td>
+        <td>{{ngReadAutoRecordDetailsDL.colour}}</td>
+        <td>{{ngReadAutoRecordDetailsDL.doILike}}</td>
+        <td>{{ngReadAutoRecordDetailsDL.popularity}}</td>
+        <td>{{ngReadAutoRecordDetailsDL.createDate}}</td>
+        <td>{{ngReadAutoRecordDetailsDL.updateDate}}</td></tr>
+    </table>
+
+
     <h2>Update</h2>
     <h3>Search Values:</h3>
     <div>
@@ -137,7 +159,71 @@
       <button id="deleteRecord">Delete Record</button>
     </div>
 
+    <h2>File Upload</h2>
+    <div>
+      <label for="fileUploadPath">Path:</label><input type="text" id="fileUploadPath" value="mcr76_hidden/">
+    </div><div>
+      <label for="fileUploadName">Name:</label><input type="text" id="fileUploadName" value="test.json">
+    </div><div>
+      <label for="fileUploadFile">File:</label><input type="file" name="fileUploadFile" id="fileUploadFile">
+    </div><div>
+      <button id="uploadFile">Upload File</button>
+    </div>
 
+    <h2>File Listing</h2>
+    <div>
+      <label for="fileListingPath">Path:</label><input type="text" id="fileListingPath" value="public_html/mcr76/fruit/">
+    </div><div>
+      <button id="listFiles">List File(s)</button>
+    </div>
+
+    <h2>File Listing (AngularJS)</h2>
+    <div>
+      <label for="ngFileListingPath">Path:</label><input ng-model="ngFileListingPath" type="text" id="ngFileListingPath">
+    </div><div>
+      <button id="ngListFiles" ng-click="ngFileListingDisplay()">List File(s)</button>
+    </div>
+    <table border="1">
+      <tr><th>File Name</th><th>File Type</th><th>File Size</th><th>Timestamp</th></tr>
+      <tr ng-repeat="row in ngFileListingDetails | orderBy:fileName">
+        <td>{{row.fileName}}</td>
+        <td>{{row.fileMime}}</td>
+        <td>{{row.fileSize | humanFileSize}}</td>
+        <td>{{row.unixTime}}</td></tr>
+    </table>
+
+    <h2>File Download</h2>
+    <form id='fileDownloadForm' method='post' action='db.php' target='_blank'>
+    <div>
+      <label for="fileDownloadPath">Path:</label><input type="text" name="fileDownloadPath" id="fileDownloadPath" value="mcr76_hidden/">
+    </div><div>
+      <label for="fileDownloadName">Name:</label><input type="text" name="fileDownloadName" id="fileDownloadName" value="test.json">
+    </div><div>
+      <input type='hidden' name='data' id="fileDownloadData" value=''>
+      <input type='hidden' name='submitting' value='YES'>
+      <button id="downloadFile">Download File</button>
+    </div>
+    </form>
+
+    <h2>File Download (AngularJS)</h2>
+    <div>
+      <label for="ngFileDownloadPath">Path:</label><input ng-model="ngFileDownloadPath" type="text" id="ngFileDownloadPath">
+    </div><div>
+      <button id="ngListDownloadFiles" ng-click="ngFileDownloadDisplay()">List Available File(s)</button>
+    </div>
+    <form id='ngFileDownloadForm' method='post' action='db.php' target='_blank'>
+    <table border="1">
+      <tr><th>File Name</th><th>File Type</th><th>File Size</th><th>Timestamp</th><th>Download</th></tr>
+      <tr ng-repeat="row in ngFileDownloadDetails | orderBy:fileName">
+        <td>{{row.fileName}}</td>
+        <td>{{row.fileMime}}</td>
+        <td>{{row.fileSize | humanFileSize}}</td>
+        <td>{{row.unixTime}}</td>
+        <td><button type="button" ng-click="ngFileDownload(row)">Download</button></td></tr>
+    </table>
+    <input type='hidden' name='data' id="ngFileDownloadData" value=''>
+    <input type='hidden' name='submitting' value='YES'>
+    </form>
 
     
   </div><!-- wrapper end -->
